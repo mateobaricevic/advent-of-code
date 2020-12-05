b1836dedf17fc825768627938f9d75fa009866a3
Mateo Baričević <35807327+mateobaricevic@users.noreply.github.com>
2020-12-06 00:19:23 +0100

    Day 1

--- day1/main.py
+++ day1/main.py
@@ -2,21 +2,21 @@ import numpy as np
 
 input = np.genfromtxt("input.txt", dtype=np.int)
 
-numbers2 = []
+numbers = []
 for i in range(0, len(input)):
     for j in range(i+1, len(input)):
         if input[i] + input[j] == 2020:
-            numbers2.append([input[i], input[j]])
+            numbers.append([input[i], input[j]])
 
-print(numbers2)
-print(numbers2[0][0]*numbers2[0][1])
+print(numbers)
+print(numbers[0][0]*numbers[0][1])
 
-numbers3 = []
+numbers2 = []
 for i in range(0, len(input)):
     for j in range(i+1, len(input)):
         for k in range(j+1, len(input)):
             if input[i] + input[j] + input[k] == 2020:
-                numbers3.append([input[i], input[j], input[k]])
+                numbers2.append([input[i], input[j], input[k]])
 
-print(numbers3)
-print(numbers3[0][0]*numbers3[0][1]*numbers3[0][2])
+print(numbers2)
+print(numbers2[0][0]*numbers2[0][1]*numbers2[0][2])
