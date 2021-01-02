79146186caec3db34811bc288d3e659a2c460c40
Mateo Baričević <35807327+mateobaricevic@users.noreply.github.com>
2021-01-02 19:14:28 +0100

    Day 21

---
+++ 2020/day21/main.py
@@ -0,0 +1,77 @@
+import pprint
+
+file = open("input.txt")
+foods = file.read().split("\n")[:-1]
+foods = [{"ingredients" : food.split(" (contains ")[0].split(" "),
+          "allergens" : food.split(" (contains ")[1].replace(")", "").split(", ")} for food in foods]
+# pprint.pprint(foods, width=58)
+
+ingredients = {}
+for food in foods:
+    for ingredient in food["ingredients"]:
+        if ingredient not in ingredients.keys():
+            ingredients[ingredient] = ''
+# print(ingredients)
+
+def countOccurances(list):
+    result = []
+    unique = set(list)
+    for item in unique:
+        count = 0
+        for i in range(len(list)):
+            if list[i] == item:
+                count += 1
+        result.append((item, count))
+    return result
+
+for _ in range(2):
+    for food in foods:
+        for allergen in food["allergens"]:
+            count = 1
+            possibilities = food["ingredients"][:]
+            for f in foods:
+                if f == food:
+                    continue
+                if allergen in f["allergens"]:
+                    possibilities += f["ingredients"]
+                    count += 1
+            counts = countOccurances(possibilities)
+            possibilities = []
+            for c in counts:
+                if count == c[1]:
+                    possibilities.append(c[0])
+            if len(possibilities) == 1:
+                ingredients[possibilities[0]] = allergen
+                for f2 in foods:
+                    for i, a in enumerate(f2["allergens"]):
+                        if a == allergen:
+                            if i < len(f2["allergens"]):
+                                f2["allergens"] = f2["allergens"][0:i] + f2["allergens"][i+1:]
+                            else:
+                                f2["allergens"] = f2["allergens"][0:i]
+                for f2 in foods:
+                    for j, i in enumerate(f2["ingredients"]):
+                        if i == possibilities[0]:
+                            if j < len(f2["ingredients"]):
+                                f2["ingredients"] = f2["ingredients"][0:j] + f2["ingredients"][j+1:]
+                            else:
+                                f2["ingredients"] = f2["ingredients"][0:j]
+            # print(allergen, possibilities)
+
+# pprint.pprint(ingredients)
+
+not_allergens = [i for i, s in ingredients.items() if s == '']
+# print(not_allergens)
+
+count = 0
+for i in not_allergens:
+    for food in foods:
+        for ingredient in food["ingredients"]:
+            if ingredient == i:
+                count += 1
+print(count)
+
+allergens = [(s, i) for i, s in ingredients.items() if s != '']
+allergens.sort()
+# print(allergens)
+print(",".join([i[1] for i in allergens]))
