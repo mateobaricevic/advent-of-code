a8ecbb0d863ea6648751f2d24c8dea284482c744
Mateo Baricevic <mbaricevic@iolap.com>
2022-01-05 10:49:38 +0100

    Complete 2021 Day 2

---
+++ 2021/day02/main.py
@@ -0,0 +1,38 @@
+# Part 1
+
+# commands = []
+# with open("2021/day02/input.txt", "r") as file:
+#   for line in file:
+#     commands.append(line.replace("\n", ""))
+
+# position = [0, 0]
+# for command in commands:
+#   instruction = command.split(" ")
+#   if instruction[0] == "forward":
+#     position[0] += int(instruction[1])
+#   elif instruction[0] == "down":
+#     position[1] += int(instruction[1])
+#   elif instruction[0] == "up":
+#     position[1] -= int(instruction[1])
+
+# print(position[0] * position[1])
+
+# Part 2
+commands = []
+with open("2021/day02/input.txt", "r") as file:
+  for line in file:
+    commands.append(line.replace("\n", ""))
+
+position = [0, 0]
+aim = 0
+for command in commands:
+  instruction = command.split(" ")
+  if instruction[0] == "forward":
+    position[0] += int(instruction[1])
+    position[1] += aim * int(instruction[1])
+  elif instruction[0] == "down":
+    aim += int(instruction[1])
+  elif instruction[0] == "up":
+    aim -= int(instruction[1])
+
+print(position[0] * position[1])
